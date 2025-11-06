9cf5b6be1da70b7d46d37cf99bd689e2afab48fd
JonDevOps <30133774+JonDevOps@users.noreply.github.com>
2025-11-06 17:38:52 +0000

    lets try option 1

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -8,6 +8,10 @@ export function initializeAdminApp(): App {
   }
 
   // When running in a Google Cloud environment, the SDK can automatically
-  // discover the service account credentials.
-  return initializeApp();
+  // discover the service account credentials. We explicitly initialize with
+  // applicationDefault and the project ID for robustness.
+  return initializeApp({
+    credential: credential.applicationDefault(),
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  });
 }
