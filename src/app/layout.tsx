9b8ab6043e2a00e2b735ce6d3f4cfe84b7371b01
JonDevOps <30133774+JonDevOps@users.noreply.github.com>
2025-11-06 17:38:48 +0000

    i dodelet the entire app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,48 +1,13 @@
-import type { Metadata } from 'next'
 import './globals.css'
-import { Toaster } from '@/components/ui/toaster'
-import { Header } from '@/components/layout/header'
-import { Footer } from '@/components/layout/footer'
-import { cn } from '@/lib/utils'
-import { FirebaseClientProvider } from '@/firebase/client-provider'
-import { Suspense } from 'react'
-
-export const metadata: Metadata = {
-  title: 'w3Develops',
-  description: 'Find Your Coding Crew',
-}
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Space+Grotesk:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body
-        className={cn('min-h-screen bg-background font-body antialiased')}
-      >
-        <FirebaseClientProvider>
-          <div className="relative flex min-h-screen flex-col">
-            <Header />
-            <main className="flex-1"><Suspense>{children}</Suspense></main>
-            <Footer />
-          </div>
-          <Toaster />
-        </FirebaseClientProvider>
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   )
 }
--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  "projectId": "studio-5652324508-ae238",
-  "appId": "1:690553774369:web:c92428e5e77642aa981e60",
-  "apiKey": "AIzaSyBeP1vJSGr9BaO1TXxqV-m0PSBKMYBYUxw",
-  "authDomain": "studio-5652324508-ae238.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "690553774369"
-};
