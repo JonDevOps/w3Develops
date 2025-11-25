3c8e3a3aba55b5d9eab30f802f9049262a348755
JonDevOps <30133774+JonDevOps@users.noreply.github.com>
2025-11-25 18:36:03 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/firebase/admin/index.ts
+++ src/firebase/admin/index.ts
@@ -22,11 +22,11 @@ if (admin.apps.length === 0) {
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     });
   }
+  firestore = getFirestore(app);
 } else {
   app = admin.apps[0]!;
+  firestore = getFirestore(app);
 }
-
-firestore = getFirestore(app);
 
 // Export the initialized instances for use in server actions.
 export { app as adminApp, firestore as adminFirestore };
