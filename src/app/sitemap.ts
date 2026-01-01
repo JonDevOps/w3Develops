6b0793460394b48d711b5c09c6d9c71f7f136ab7
JonDevOps <30133774+JonDevOps@users.noreply.github.com>
2026-01-01 09:04:42 +0000

    instead of this page being contribute/intro just have it be /contribute

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -13,7 +13,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
     '/competitions',
     '/cookies',
     '/contribute',
-    '/contribute/intro',
     '/donate',
     '/explore',
     '/feedback',
