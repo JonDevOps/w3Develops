e16a5cb1509ff30546c65b8d0d9ec6062bc2fb42
JonDevOps <30133774+JonDevOps@users.noreply.github.com>
2026-02-05 10:44:01 +0000

    i approve

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -24,7 +24,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
     '/feedback',
     '/groupprojects',
     '/groupprojects/create',
-    '/hackathon',
     '/hackathons',
     '/hackerspaces',
     '/job-board',
