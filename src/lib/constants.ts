7ca123eaecb95141da4a668cf3971bd7f86841ee
JonDevOps <30133774+JonDevOps@users.noreply.github.com>
2026-02-06 05:27:23 +0000

    remove fiction, non-fiction, scofi, fantasy,mystery,biography,hostory,an

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,7 +5,6 @@ export const topics = [
   "HTML/CSS", "JavaScript", "Python", "React", "Django", "Node.js", "Rust", 
   "Digital Marketing", "Web3", "Cryptocurrency", "Cybersecurity", "NFTs", "SQL", 
   "Artificial Intelligence", "Web Design", "Programming Fundamentals",
-  "Fiction", "Non-Fiction", "Sci-Fi", "Fantasy", "Mystery", "Biography", "History", "Self-Help",
   "Other"
 ];
 
